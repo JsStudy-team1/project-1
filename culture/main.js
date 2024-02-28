eb8414d981ec78183bcd7ecade95be1534aef0bd
noel-vibe <138129056+noel-vibe@users.noreply.github.com>
2024-02-29 01:09:48 +0900

    json

--- culture/main.js
+++ culture/main.js
@@ -1,2 +1,8 @@
-let API_KEY = ''
-let url = ''
+const getDataApi = async() => {
+    const data = await fetch('test.json')
+    const data1 = await data.json()
+    console.log("data",data)
+
+}
+
+getDataApi()
