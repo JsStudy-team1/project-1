650c7d477ff49315a67aa526c98f4ddc58c01424
noel-vibe <138129056+noel-vibe@users.noreply.github.com>
2024-03-02 08:13:15 +0900

    카드야 정신차려

--- culture/culture.js
+++ culture/culture.js
@@ -1,5 +1,8 @@
 const API_KEY = '724c4f436f6777613735434441514d'
 let cultureList = []
+const menus = document.querySelectorAll(".menus button");
+
+menus.forEach(menu=>menu.addEventListener("click",(event)=>getCultureByCategory(event)))
 
 //받은 json파일 사용
 // const getDataApi = async() => {
@@ -10,25 +13,50 @@ let cultureList = []
 // }
 // getDataApi()
 
+
+
 const getLatestCulture = async () =>{
     const url = new URL(`http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/20`)
     const response = await fetch(url)
     const data = await response.json()
     cultureList = data.culturalEventInfo.row
-    
-    console.log("data",cultureList)
-render();
+    render()
 };
+
+const getCultureByCategory =async (event) => {
+    const category = event.target.textContent
+    const url = new URL(`http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/20/${category}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log("ddd",data)
+    cultureList = data.culturalEventInfo.row
+    render()
+}
+
+const getCultureByKeyword = async () => {
+    const keyword = document.getElementById("search-input").value
+    const url = new URL(`http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/20/${keyword}/${keyword}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log("keyword",data)
+    cultureList = data.culturalEventInfo.row
+    render()
+}
 
 const render = () =>{
     let cultureHTML = cultureList.map((item) => 
-        `<div class ="row">
-            <div class="card col-lg-4 col-sm-12" style="width: 18rem;">
-                <img src="${item.MAIN_IMG}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${item.TITLE}</h5>
-                    <p class="card-text">${item.USE_TRGT}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        `<div class ="row row-cols-1 row-cols-md-3 g-4">
+            <div class = "col">
+                <div class="card">
+                    <img src="${item.MAIN_IMG}" class="card-img-top img-fluid" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">[${item.CODENAME}] ${item.TITLE}</h5>
+                        <p class="card-text">
+장소 : ${item.PLACE}
+날짜/시간 : ${item.DATE}
+이용대상 : ${item.USE_TRGT}</p>
+                        <a href="${item.ORG_LINK}" class="card-link">홈페이지 바로가기</a>
+                    </div>
                 </div>
             </div>
         </div>`
@@ -48,7 +76,7 @@ getLatestCulture()
 // START_INDEX	INTEGER(필수)	요청시작위치	정수 입력 (페이징 시작번호 입니다 : 데이터 행 시작번호)
 // END_INDEX	INTEGER(필수)	요청종료위치	정수 입력 (페이징 끝번호 입니다 : 데이터 행 끝번호)
 // CODENAME	STRING(선택)	분류
-//CODENAME에는 문화교양/강좌,전시/미술,뮤지컬/오페라,기타,연극,무용,영화,
+//CODENAME에는 교육/체험,전시/미술,뮤지컬/오페라,기타,연극,무용,영화,
 //국악,콘서트,축제-문화/예술,축제-전통/역사,축제-시민화합,클래식,축제-기타,축제-자연/경관,독주/독창회로 총16개	
 // TITLE	STRING(선택)	공연/행사명	
 // DATE	STRING(선택)	날짜/시간	YYYY-MM-D
@@ -57,6 +85,6 @@ getLatestCulture()
 //예) http://openapi.seoul.go.kr:8088/sample/xml/culturalEventInfo/1/5/ /카즈미
 
 
-//실내 = 문화교양/강좌,전시/미술,뮤지컬/오페라,기타,연극,무용,영화,국악,콘서트,클래식,독주/독창회
+//실내 = 교육/체험,전시/미술,뮤지컬/오페라,기타,연극,무용,영화,국악,콘서트,클래식,독주/독창회
 
 //실외 = 축제-문화/예술,축제-전통/역사,축제-시민화합,축제-기타,축제-자연/경관
