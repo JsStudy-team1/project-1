c0e779af264539a762c794bee5f32b7aefea50f0
gyangtto <105619361+gyangtto@users.noreply.github.com>
2024-03-03 12:57:31 +0900

    메인추가

--- slide/slide.js
+++ slide/slide.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   new Swiper('.mySwiper', {
     slidesPerView: 1,
     loop: true,
+    autoplay: {
+      delay: 2000 // 시간 설정
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
