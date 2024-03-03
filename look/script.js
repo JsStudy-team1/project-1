24f9e85110f09393edd91d736f331193de6c5705
narii <madnarii@gmail.com>
2024-03-03 11:57:59 +0900

    feat 옷추천, 비, 한파 폭염주의보

--- look/script.js
+++ look/script.js
@@ -5,67 +5,130 @@ function fetchWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('네트워크 응답이 올바르지 않습니다.');
-        }
-        return response.json(); 
-      })
-      .then(data => {
-        const tempInCelsius = data.main.temp - 273.15;
-        document.getElementById('temperature').textContent = tempInCelsius.toFixed(2);
-        
-        // 아이콘 정보를 가져와서 이미지 URL을 구성
-        const iconCode = data.weather[0].icon;
-        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
-        
-        // 이미지 태그의 src 속성을 업데이트
-        document.getElementById('weather-icon').src = iconUrl;
-        let imageName; // 온도에 따른 이미지 이름 초기화
-        switch (true) {
-            case (tempInCelsius <= 0):
-                imageName = 'boots.png'; // 매우 추운 날씨
-                break;
-            case (tempInCelsius > 0 && tempInCelsius <= 5):
-                imageName = 'hoodJacket.png'; // 쌀쌀한 날씨
-                break;
-            case (tempInCelsius > 0 && tempInCelsius <= 10):
-              imageName = 'longbeanie.png'; // 쌀쌀한 날씨
-              break;
-            case (tempInCelsius > 0 && tempInCelsius <= 15):
-              imageName = 'skirt.png'; // 쌀쌀한 날씨
-              break;
-            case (tempInCelsius > 10 && tempInCelsius <= 20):
-                imageName = 'coat.png'; // 따뜻한 날씨
-                break;
-            case (tempInCelsius > 20):
-                imageName = 'halfShirt.png'; // 더운 날씨
-                break;
-            default:
-                imageName = 'hoodie.png'; // 기본 이미지
-        }
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('네트워크 응답이 올바르지 않습니다.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const tempInCelsius = data.main.temp - 273.15;
+            document.getElementById('temperature').textContent = tempInCelsius.toFixed(2);
 
-        // 조건에 따른 이미지 URL 구성 및 이미지 태그의 src 속성 업데이트
-        const imageUrl = `./images/${imageName}`;
-        document.getElementById('condition-icon').src = imageUrl; // 'condition-icon'은 조건에 따른 이미지를 표시할 요소의 ID
-    })
-    .catch(error => {
-        console.error('요청 중 문제가 발생했습니다:', error);
-        document.getElementById('temperature').textContent = 'Error';
-    });
+            const weatherConditions = data.weather;
+            const isRaining = weatherConditions.some(condition => condition.main === 'Rain' || condition.id >= 500 && condition.id <= 531);
+            
+            const iconCode = data.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+
+            document.getElementById('weather-icon').src = iconUrl;
+            let items;
+            let recomendItems = [];
+            switch (true) {
+                case (tempInCelsius <= 0):
+                    items = 'longPadding.png'; // 매우 추운 날씨
+                    recomendItems = ['longbeanie.png', 'boots.png', 'gloves.png'];
+                    break;
+                case (tempInCelsius > 0 && tempInCelsius <= 5):
+                    items = 'hoodJacket.png'; // 추운 날씨
+                    recomendItems = ['longbeanie.png', 'longPadding.png', 'gloves.png'];
+                    break;
+                case (tempInCelsius > 5 && tempInCelsius <= 10):
+                    items = 'jacket.png'; // 쌀쌀한 날씨
+                    recomendItems = ['hoodie.png', 'longknit.png', 'longbeanie.png'];
+                    break;
+                case (tempInCelsius > 10 && tempInCelsius <= 15):
+                    items = 'coat.png'; // 일교차 심한 날씨
+                    recomendItems = ['jacket.png', 'muslin.png', 'hoodie.png'];
+                    break;
+                case (tempInCelsius > 15 && tempInCelsius <= 20):
+                    items = 'longknit.png'; // 따뜻한 날씨
+                    recomendItems = ['hoodie.png', 'longShirt.png', 'skirt.png'];
+                    break;
+                case (tempInCelsius > 20 && tempInCelsius <= 25):
+                    items = 'halfShirt.png'; // 따뜻한 날씨
+                    recomendItems = ['halfSetupNormal.png', 'mansHalfSetup.png', 'halfblouse.png'];
+                    break;
+                case (tempInCelsius > 25 && tempInCelsius <= 30):
+                    items = 'halfblouse.png'; // 약간 더운 날씨
+                    recomendItems = ['halfShirt.png', 'halfPants.png', 'skirt.png'];
+                break;
+                case (tempInCelsius > 30 && tempInCelsius <= 33):
+                    items = 'halfShirtMan.png'; // 더운 날씨
+                    recomendItems = ['tnak-top.png', 'bra.png', 'undershirt.png'];
+                break;
+                case (tempInCelsius > 30):
+                    items = 'glasses.png'; // 개더운 날씨
+                    recomendItems = ['bikini.png', 'undershirt.png', 'halfPants.png'];
+
+                    break;
+                default:
+                    items = 'hoodie.png'; // 기본 이미지
+            }
+            const rainingElement = document.getElementById('raining');
+            if (isRaining) {
+                rainingElement.innerHTML = '<i class="fa-solid fa-face-sad-tear"></i> 비가 오고 있습니다! 우산을 챙기세요.';
+            } else {
+                rainingElement.innerHTML = '<i class="fa-solid fa-dog"></i> 댕댕이 산책 가능';
+            }
+                    
+            const messageElement = document.getElementById("weather-message");
+
+            if (tempInCelsius < 0) {
+              messageElement.textContent = "한파주의보. 외출을 삼가하세요.";
+              messageElement.classList.add("cold-message");
+              messageElement.classList.remove("hot-message");
+            } else if (tempInCelsius > 25) {
+              messageElement.textContent = "폭염주의보. 외출을 삼가하세요.";
+              messageElement.classList.add("hot-message");
+              messageElement.classList.remove("cold-message");
+            } else {
+              messageElement.textContent = "";
+              messageElement.classList.remove("cold-message", "hot-message");
+            }
+            
+            const imageUrl = `./images/${items}`;
+            document.getElementById('condition-icon').src = imageUrl;
+
+            const recomendList = document.getElementById('recomend-list');
+            recomendList.innerHTML = '';
+
+            recomendItems.forEach(item => {
+                const img = document.createElement('img');
+                img.src = `./images/${item}`;
+                img.alt = 'Recomend item';
+                recomendList.appendChild(img); 
+            });
+        })
+        .catch(error => {
+            console.error('요청 중 문제가 발생했습니다:', error);
+            document.getElementById('temperature').textContent = 'Error';
+        });
 }
 
+    //옷 추천하기 버튼 누르면 보여주자
+    document.addEventListener("DOMContentLoaded", function() {
+        document.querySelector(".recomend-btn").addEventListener("click", function() {
+            var clothesContainer = document.querySelector(".container.clothes");
 
-// 셀렉트 메뉴 값 변경 시 날씨 정보 업데이트
-document.getElementById('city-select').addEventListener('change', function() {
-    // '지역 선택' 옵션이 선택된 경우가 아니라면 날씨 정보를 표시
-    if (this.value !== '지역 선택') {
-        document.getElementById('weather').style.display = 'block'; // 또는 'flex', 상황에 맞게
-        fetchWeather(this.value);
-    } else {
-        // '지역 선택'이 다시 선택된 경우 날씨 영역을 숨김
-        document.getElementById('weather').style.display = 'none';
-    }
+            if (clothesContainer.style.display === "none" || clothesContainer.style.display === "") {
+                clothesContainer.style.display = "block"; 
+            } else {
+                clothesContainer.style.display = "none";
+            }
+        });
+
+        //지역선택 전엔 하위 container 모두 숨김
+    document.getElementById('city-select').addEventListener('change', function() {
+        if (this.value !== '지역 선택') {
+            document.getElementById('weather').style.display = 'block';
+            document.getElementById("clothes").style.display = 'none';
+            fetchWeather(this.value);
+        } else {
+            document.getElementById('weather').style.display = 'none';
+            document.querySelector(".container.clothes").style.display = 'none';
+        }
+    });
 });
 
 
